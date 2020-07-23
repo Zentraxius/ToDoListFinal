82a970cc4d3e750961b2f07fcf90f17cac62e25c
Cody Fritz <clanalia55@gmail.com>
2020-07-23 14:18:36 -0700

    update ToDoList with new testing and features
    Co-authored-by:  Joseph Pearce <joseph.pearce05@gmail.com>

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -7,16 +7,17 @@ namespace ToDoList.Controllers
   public class ItemsController : Controller
   {
 
-    [HttpGet("/items")]
-    public ActionResult Index()
-    {
-      List<Item> allItems = Item.GetAll();
-      return View(allItems);
-    }
+    // [HttpGet("/items")]
+    // public ActionResult Index()
+    // {
+    //   List<Item> allItems = Item.GetAll();
+    //   return View(allItems);
+    // }
 
-    [HttpGet("/items/new")]
-    public ActionResult New()
+    [HttpGet("/categories/{categoryId}/items/new")]
+    public ActionResult New(int categoryId)
     {
+      Category category = Category.Find(categoryId);
       return View();
     }
 
@@ -39,6 +40,17 @@ namespace ToDoList.Controllers
     {
       Item foundItem = Item.Find(id);
       return View(foundItem);
+    }
+
+    [HttpGet("/categories/{categoryId}/items/{itemId}")]
+    public ActionResult Show(int categoryId, int itemId)
+    {
+      Item item = Item.Find(itemId);
+      Category category = Category.Find(categoryId);
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      model.Add("item", item);
+      model.Add("category", category);
+      return View(model);
     }
   }
 }
